d7f7115c4c2908fc33bc132c4ec289d2dd5cc77e
wilPoly <misterwrinkle@gmail.com>
2020-04-17 17:38:33 +0200

    Add player movement and animation left-right

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mini-jam_52
- Entry for the 52nd Mini Jam - Summit
+Entry for the [52nd Mini Jam - Summit](https://itch.io/jam/mini-jam-52-summit)
---
+++ actors/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+export (int) var speed := 200
+export (int) var gravity := 1200
+
+var direction := Vector2()
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	animate_player(direction)
+	move_and_slide(direction, Vector2(0, -1))
+
+func get_input() -> void:
+	direction.x = (Input.get_action_strength("arrow_right") - Input.get_action_strength("arrow_left")) * speed
+
+
+func animate_player(direction: Vector2):
+	var direction_norm = direction.normalized()
+	if direction_norm.x > 0:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+	elif direction_norm.x < 0:
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
